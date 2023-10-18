a8b8eaf209869c1a5991e7a533b9dae20a453782
Huio-op <fe151214@gmail.com>
2023-10-18 20:38:44 -0300

    #4 Make the Account Creation screen - Making it pretty for presentation

--- src/app/bottomTabNavigation/Feed.js
+++ src/app/bottomTabNavigation/Feed.js
@@ -39,6 +39,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 10,
+    height: '100%',
   },
   profileWrapper: {
     flexDirection: 'row',
